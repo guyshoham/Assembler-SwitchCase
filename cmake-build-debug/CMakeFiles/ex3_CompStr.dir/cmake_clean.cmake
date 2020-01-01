b4cc7875e30d7f84d64d9befb5c2acf655fc88ea
Guy Shoham <guyshoham28@gmail.com>
2020-01-01 11:10:16 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/ex3_CompStr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3_CompStr.dir/main.c.o"
+  "ex3_CompStr"
+  "ex3_CompStr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex3_CompStr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
